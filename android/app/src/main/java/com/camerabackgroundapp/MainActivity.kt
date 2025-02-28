19bd5ba9fcda982dc310c54e6db2c000b4a3109f
renan <renanmakoto@outlook.com>
2025-02-28 09:02:19 -0500

    Everything rebuilt

--- android/app/src/main/java/com/camerabackgroundapp/MainActivity.kt
+++ android/app/src/main/java/com/camerabackgroundapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "CameraBackgroundApp"
+  override fun getMainComponentName(): String = "cameraBackgroundApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
