7b0ca0371c4d07c1a85e87c63673338cab518e52
user <renanmakoto@outlook.com>
2025-06-20 10:50:24 -0400

    Build successfully done

---
+++ android/app/.cxx/RelWithDebInfo/4xq5z6u1/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-vision-camera::VisionCamera)
+add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
+set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
+    IMPORTED_LOCATION "/home/user/Desktop/cameraBackgroundApp/node_modules/react-native-vision-camera/android/build/intermediates/cxx/RelWithDebInfo/53n1e312/obj/x86/libVisionCamera.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/user/Desktop/cameraBackgroundApp/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
